9b1b58c115405cb2a36b32d3ac15c29c9867c22d
Shoykat361 <shoykat15-11724@diu.edu.bd>
2021-03-25 01:30:52 +0600

    Added new method this is new

--- hello.java
+++ hello.java
@@ -2,5 +2,9 @@ public class Hello {
     public static void main (string[] args){
         System.out.println("Vs code is ossam")
     }
+    public static void main(){
+         System.out.println("i am new ")
+
+    }
 
 }
